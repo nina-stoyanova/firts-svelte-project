44768c2cadf700856a461f795c3148eae0d2272d
nina.stoyanova <nina.stoyanova@egt-digital.com>
2023-01-20 13:04:25 +0200

    added delete item feature

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,33 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  // @ts-nocheck
+
+  import "../public/global.css";
+  import FeedbackList from "./components/FeedbackList.svelte";
+
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+  ];
+
+  const deleteFeedback = (e) => {
+    const itemId = e.detail;
+    feedback = feedback.filter((item) => item.id !== itemId);
+  };
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
+<main class="container">
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,15 @@
+<script>
+  // @ts-nocheck
+
+  import FeedbackItem from "./FeedbackItem.svelte";
+
+  export let feedback;
+
+  console.log(feedback);
+</script>
+
+{#each feedback as feed (feed.id)}
+  <FeedbackItem item={feed} on:delete-feedback />//catch the custon event
+{/each}
+
+FeedbackList
